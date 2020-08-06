6b867d998b1e8569fcdd72696e864dd3522c3cc6
Andre Padeti <andrepadeti@hotmail.com>
2020-08-05 23:17:04 -0300

    everything is function but it doesn't work the way it should

--- src/sentenceComponent.js
+++ src/sentenceComponent.js
@@ -1,76 +1,63 @@
-import React, { Component } from "react";
-import { ReactSortable } from "react-sortablejs";
-import { MDBContainer, MDBRow, MDBCol, MDBBox } from "mdbreact";
+import React, { useState } from "react"
+import { ReactSortable } from "react-sortablejs"
+import { MDBContainer, MDBRow, MDBCol, MDBBox } from "mdbreact"
 
-export default class Sentence extends Component {
-  constructor(props) {
-    super (props);
+export default function Sentence(props) {
   
-    // prepare this.state.list to be used by SortableJS
-    let id = 1;
-    let result = [];
-    this.props.sentenceShuffled.forEach((word) => {
-      result.push({ id: id.toString(), name: word, class: 'sortable'});
-      id++;
-    });
+  // prepare list to be used by SortableJS
+  let id = 1
+  let result = []
+  props.sentenceShuffled.forEach((word) => {
+    result.push({ id: id.toString(), name: word, class: 'sortable'})
+    id++
+  });
 
-    this.state = {
-      sentenceInOrder: this.props.sentenceInOrder, // array that stores the split sentence
-      sentenceShuffled: this.props.sentenceShuffled, // array that stores the shuffled sentence
-      currentCorrectWord: 0, // stores how many words are correct already while student is sorting
-      currentlyLeft: [], // array that stores the words still left to be sorted
-      complete: false, // becames true when sentence is sorted
-      list: result // this object is used by SortableJS and is calculated in componentWillMount
-    };
-  
-    this.handleDragEnd = this.handleDragEnd.bind(this);
-  }
+  const [ list, setList ] = useState(result)
 
-  handleDragEnd() {
-    let currentCorrectWord = this.state.currentCorrectWord;
+  let complete = false
+  let currentCorrectWord = 0
+
+  const handleDragEnd = () => {
     
-    while ( this.state.list[currentCorrectWord].name === this.state.sentenceInOrder[currentCorrectWord]) {
-      let newList = this.state.list;
-      newList[currentCorrectWord].class = 'notSortable';
-      currentCorrectWord++;
-      this.setState({ currentCorrectWord: currentCorrectWord });
-      this.setState({ list: newList });
+    while ( list[currentCorrectWord].name === props.sentenceInOrder[currentCorrectWord]) {
+      let newList = list
+      newList[currentCorrectWord].class = 'notSortable'
+      currentCorrectWord++
+      setList(newList);
 
-      if ( currentCorrectWord === this.state.sentenceInOrder.length  ) {
-        this.props.onCompletion(this.props.number);
-        this.setState((state, props) => (
-          { complete: true } ));
-          let sentenceCompleteAudio = new Audio("bell.wav");
-          sentenceCompleteAudio.play();
+      if ( currentCorrectWord === props.sentenceInOrder.length  ) {
+        props.onCompletion(props.number);
+        complete = true
+        let sentenceCompleteAudio = new Audio("bell.wav");
+        sentenceCompleteAudio.play();
         break;
       }
     }
   }
 
-  render() {
-    return (
-      <MDBRow>
-        <MDBContainer className='sentenceContainer'>
-          <MDBCol size='1' className="sentenceNumber">
-            <p>{this.props.number}</p> 
-          </MDBCol>
-          <MDBCol size='10' className="sentence">
-            <ReactSortable
-              list={this.state.list}
-              setList={newState => this.setState({ list: newState })}
-              filter=".notSortable"
-              // draggable=".sortable"
-            >
-              {this.state.list.map(item => (
-                <MDBBox className={item.class + ' eachWord'} key={item.id} onDragEnd={this.handleDragEnd} >{item.name}</MDBBox>
-              ))}
-            </ReactSortable>
-          </MDBCol>
-          <MDBCol size='1' className="check">
-              <img className="checkIcon" alt="" src={this.state.complete ? "tick.png" : ""}/>
-          </MDBCol>
-        </MDBContainer>
-      </MDBRow>
-    );
-  }
+  return (
+    <MDBRow>
+      <MDBContainer className='sentenceContainer'>
+        <MDBCol size='1' className="sentenceNumber">
+          <p>{props.number}</p> 
+        </MDBCol>
+        <MDBCol size='10' className="sentence">
+          <ReactSortable
+            list={list}
+            setList={setList}
+            filter=".notSortable"
+            // draggable=".sortable"
+          >
+            {list.map(item => (
+              // OR className={item.class + ' eachWord'} below is the template literals (strings) implementation
+              <MDBBox className={`${item.class} eachWord`} key={item.id} onDragEnd={handleDragEnd} >{item.name}</MDBBox>
+            ))}
+          </ReactSortable>
+        </MDBCol>
+        <MDBCol size='1' className="check">
+            <img className="checkIcon" alt="" src={complete ? "tick.png" : ""}/>
+        </MDBCol>
+      </MDBContainer>
+    </MDBRow>
+  );
 }
